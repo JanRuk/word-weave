b1775ea84b627bbd4ab84d8b117d879824f0b984
Janinda Rukshan <janruk91@gmail.com>
2025-02-14 22:26:17 +0530

    Add method to disable rows to MainSceneController

--- src/main/java/com/github/JanRuk/controller/MainSceneController.java
+++ src/main/java/com/github/JanRuk/controller/MainSceneController.java
@@ -2,10 +2,14 @@ package com.github.JanRuk.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+
+import java.util.ArrayList;
 
 public class MainSceneController {
 
@@ -23,8 +27,16 @@ public class MainSceneController {
     public TextField txtR4C1, txtR4C2, txtR4C3, txtR4C4, txtR4C5;
     public TextField txtR5C1, txtR5C2, txtR5C3, txtR5C4, txtR5C5;
     public TextField txtR6C1, txtR6C2, txtR6C3, txtR6C4, txtR6C5;
+    public HBox hbxRow1;
+    public HBox hbxRow3;
+    public HBox hbxRow2;
+    public HBox hbxRow4;
+    public HBox hbxRow5;
+    public HBox hbxRow6;
+    public Label lblWinStreak;
 
     public void initialize() {
+        disableRows(hbxRow2, hbxRow3, hbxRow4, hbxRow5, hbxRow6);
     }
 
     public void btnClearOnAction(ActionEvent event) {
@@ -37,6 +49,16 @@ public class MainSceneController {
 
     public void btnSubmitOnAction(ActionEvent event) {
 
+    }
+
+    public void disableRows(HBox... hbxRow) {
+        for (HBox hbox: hbxRow) {
+            for (Node node: hbox.getChildren()) {
+                if (node instanceof TextField) {
+                    node.setDisable(true);
+                }
+            }
+        }
     }
 
 }
