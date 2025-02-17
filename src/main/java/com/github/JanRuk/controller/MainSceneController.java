5961710e332c9d6eded36df7588e64cb8d574000
Janinda Rukshan <janruk91@gmail.com>
2025-02-18 00:45:13 +0530

    Implement game logic

--- src/main/java/com/github/JanRuk/controller/MainSceneController.java
+++ src/main/java/com/github/JanRuk/controller/MainSceneController.java
@@ -2,10 +2,7 @@ package com.github.JanRuk.controller;
 
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
@@ -14,10 +11,12 @@ import javafx.scene.layout.HBox;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Optional;
 import java.util.Random;
 
 public class MainSceneController {
 
+    public static String wordToGuess = generateWord();
     public AnchorPane anchrpnTitle;
     public Button btnClear;
     public Button btnExit;
@@ -26,12 +25,6 @@ public class MainSceneController {
     public Label lblG, lblH, lblI, lblJ, lblK, lblL;
     public Label lblM, lblN, lblO, lblP, lblQ, lblR, lblS, lblT, lblU, lblV, lblW, lblX, lblY, lblZ;
     public Label lblTitle;
-    public TextField txtR1C1, txtR1C2, txtR1C3, txtR1C4, txtR1C5;
-    public TextField txtR2C1, txtR2C2, txtR2C3, txtR2C4, txtR2C5;
-    public TextField txtR3C1, txtR3C2, txtR3C3, txtR3C4, txtR3C5;
-    public TextField txtR4C1, txtR4C2, txtR4C3, txtR4C4, txtR4C5;
-    public TextField txtR5C1, txtR5C2, txtR5C3, txtR5C4, txtR5C5;
-    public TextField txtR6C1, txtR6C2, txtR6C3, txtR6C4, txtR6C5;
     public HBox hbxRow1;
     public HBox hbxRow3;
     public HBox hbxRow2;
@@ -39,20 +32,23 @@ public class MainSceneController {
     public HBox hbxRow5;
     public HBox hbxRow6;
     public Label lblWinStreak;
-
-    public final String wordToGuess = generateWord();
     public StringBuilder currentWord = new StringBuilder();
     public AnchorPane root;
     public GridPane grdRows;
+    public TextField txtR1C1;
     HBox activeHbox;
 
     public void initialize() {
         disableRows(hbxRow2, hbxRow3, hbxRow4, hbxRow5, hbxRow6);
-        setTextFieldsToSingleLetters(hbxRow1,hbxRow2, hbxRow3, hbxRow4, hbxRow5,hbxRow6);
+        setTextFieldsToSingleLetters(hbxRow1, hbxRow2, hbxRow3, hbxRow4, hbxRow5, hbxRow6);
         System.out.println(wordToGuess);
     }
 
     public void btnSubmitOnAction(ActionEvent event) {
+        playGame();
+    }
+
+    private void playGame() {
         for (Node node : grdRows.getChildren()) {
             if (node instanceof HBox hbox) {
                 if (isActiveHBox(hbox)) {
@@ -62,24 +58,33 @@ public class MainSceneController {
             }
         }
         for (Node node : activeHbox.getChildren()) {
-            if (node instanceof TextField) currentWord.append(((TextField) node).getText());
+            if (node instanceof TextField textField) currentWord.append(textField.getText());
         }
         System.out.println(currentWord.toString());
-
         String guessedWord = currentWord.toString().toUpperCase();
-
         if (guessedWord.equals(wordToGuess)) {
-            new Alert(Alert.AlertType.INFORMATION, "You guessed correctly").show();
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Your guess is Correct.Do you want to continue?", ButtonType.YES, ButtonType.NO);
+            Optional<ButtonType> buttonType = alert.showAndWait();
+            if (buttonType.isPresent() && buttonType.get() == ButtonType.YES) {
+                disableRows(hbxRow2, hbxRow3, hbxRow4, hbxRow5, hbxRow6);
+                setTextFieldsToSingleLetters(hbxRow1, hbxRow2, hbxRow3, hbxRow4, hbxRow5, hbxRow6);
+                cleanText();
+                setHBoxDisabled(hbxRow1, false);
+                txtR1C1.requestFocus();
+            } else if (buttonType.get() == ButtonType.NO) {
+                btnExit.fire();
+            }
+
         } else {
             moveToNextHBox(activeHbox);
+            currentWord = new StringBuilder();
         }
     }
 
     private void moveToNextHBox(HBox activeHbox) {
         boolean foundCurrent = false;
         for (Node node : grdRows.getChildren()) {
-            if (node instanceof HBox) {
-                HBox hbox = (HBox) node;
+            if (node instanceof HBox hbox) {
                 if (foundCurrent) {
                     setHBoxDisabled(hbox, false);
 
@@ -99,7 +104,6 @@ public class MainSceneController {
     }
 
     public void btnClearOnAction(ActionEvent event) {
-
     }
 
     public void btnExitOnAction(ActionEvent event) {
@@ -116,7 +120,7 @@ public class MainSceneController {
         }
     }
 
-    private String generateWord() {
+    private static String generateWord() {
         try {
             String content = new String(Files.readAllBytes(Paths.get("/home/janinda/Documents/dep-13/projects/word-weave/src/main/resources/word/word-list.txt")));
             String[] words = content.split(",");
@@ -154,8 +158,8 @@ public class MainSceneController {
             }
         }
     }
-
     // Enable or disable all text fields in a HBox
+
     private void setHBoxDisabled(HBox hbox, boolean disabled) {
         for (Node node : hbox.getChildren()) {
             if (node instanceof TextField) {
@@ -170,6 +174,16 @@ public class MainSceneController {
             if (node instanceof TextField && !((TextField) node).isDisable()) return true;
         }
         return false;
+    }
+
+    private void cleanText() {
+        for (Node node : grdRows.getChildren()) {
+            if (node instanceof HBox hbox) {
+                for (Node child : hbox.getChildren()) {
+                    if (child instanceof TextField) ((TextField) child).clear();
+                }
+            }
+        }
     }
 
 }
